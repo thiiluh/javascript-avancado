e4e477559e989be1fef5ebe0eb23fb2602867145
Thiago Oliveira <thiago.oliveira@matera.com>
2019-02-08 16:51:46 -0200

    Controller Negociacao

---
+++ aluraframe/client/js/app/controllers/ControllerNegociacao.js
@@ -0,0 +1,32 @@
+class ControllerNegociacao {
+  constructor() {
+    let $ = document.querySelector.bind(document);
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
+    this._listaNegociacoes = new ListaNegociacoes();
+  }
+
+  adiciona(event) {
+    event.preventDefault();
+
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaForm();
+    console.log(this._listaNegociacoes.negociacoes);
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.TextoParaData(this._inputData.value),
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+  }
+
+  _limpaForm() {
+    this._inputData.value = "";
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = 0.0;
+    this._inputData.focus();
+  }
+}
