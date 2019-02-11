2eb76b8cc02dcb35e515bd67f0b5d075655032dd
Thiago Oliveira <thiago.oliveira@matera.com>
2019-02-11 08:42:47 -0200

    Alterando Regex

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -7,7 +7,7 @@ class DateHelper {
     return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
   }
   static TextoParaData(texto) {
-      if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('Deve estar no formato aaaa-mm-dd');
+      if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)) throw new Error('Deve estar no formato aaaa-mm-dd');
     return new Date(
       ...texto.split("-").map((item, indice) => item - (indice % 2))
     );
